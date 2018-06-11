ef21c493c7f2e73c3fe57d5ba35a111159b134e7
Pierre Cadieux <pierre.cadieux@quebecormedia.com>
2018-06-11 07:46:42 -0400

    games au lieu de match, ajout des Lambdas et du gw - fonctionne pas encore

---
+++ ajoutMatch.ps1
@@ -0,0 +1,10 @@
+components insert --table games --itemData '{ "Endroit": "Mont-St-Hilaire", "DateHeure": "2018-05-15 19:00", "EquipeLoc": "Express", "EquipeVis": "Ste-Julie", "Sport": "Baseball", "PtsLoc": "2", "PtsVis": "17"}'
+components insert --table games --itemData '{
+    "Endroit": "Mont-St-Hilaire",
+    "DateHeure": "2018-05-2- 13:00",
+    "EquipeLoc": "St-Hubert",
+    "EquipeVis": "Express",
+    "Sport": "Baseball",
+    "PtsLoc": "1",
+    "PtsVis": "4"
+  }'
---
+++ code/games.js
@@ -0,0 +1,76 @@
+const AWS = require('aws-sdk')
+const dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
+
+const tableName = process.env.gamesTableName
+
+function create(evt, ctx, cb) {
+  const item = JSON.parse(evt.body)
+  dynamo.put({
+    Item: item,
+    TableName: tableName
+  }, (err, resp) => {
+    if (err) {
+      cb(err)
+    } else {
+      cb(null, {
+        statusCode: 201,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(resp)
+      })
+    }
+  })
+}
+
+
+function get(evt, ctx, cb) {
+  const vId = parseInt(evt.pathParameters.id, 10)
+  dynamo.get({
+    Key: {
+      id: vId
+    },
+    TableName: tableName
+  }, (err, data) => {
+    if (err) {
+      cb(err)
+    } else {
+      const game = data.Item
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(game)
+      })
+    }
+  })
+}
+
+function list(evt, ctx, cb) {
+  dynamo.scan({
+    TableName: tableName
+  }, (err, data) => {
+    if (err) {
+      cb(err)
+    } else {
+      const games = data.Items
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(games)
+      })
+    }
+  })
+}
+
+module.exports = {
+  create,
+  get,
+  list
+}
